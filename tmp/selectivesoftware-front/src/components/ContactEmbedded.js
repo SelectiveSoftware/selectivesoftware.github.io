93961aec4d226406db5a87444d427d7e54fe46fe
Scott <scottmillan1@gmail.com>
2020-02-19 17:35:38 +0000

    cleanuped some stuf

--- tmp/selectivesoftware-front/src/components/ContactEmbedded.js
+++ tmp/selectivesoftware-front/src/components/ContactEmbedded.js
@@ -1,6 +1,5 @@
 import React from 'react'
 import TextField from '@material-ui/core/TextField';
-import { makeStyles } from '@material-ui/core/styles';
 
 class ContactEmbedded extends React.Component {
     constructor(props) {
