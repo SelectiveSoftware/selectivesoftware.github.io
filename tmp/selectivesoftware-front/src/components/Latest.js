c1d6f797b664be6ce957044098ca387228084996
Scott <scottmillan1@gmail.com>
2020-03-17 10:36:37 +0000

    updated latest to use grid wrapper

--- tmp/selectivesoftware-front/src/components/Latest.js
+++ tmp/selectivesoftware-front/src/components/Latest.js
@@ -3,11 +3,17 @@ import React from 'react'
 class Latest extends React.Component {
     render() {
         return (
-            <div className="Latest">
-                <h2>Latest Project</h2>
-                <p>This client was looking for a static website. Our process involved drawing up inital various designs for the client, before getting feedback and working on the designs interatively.</p>
-                <p>Once the design phase was complete, we built and hosted the website for the client.</p>
-                <p>During the process of understanding the clients business, we suggested other ideas where technology could help the clients business. A couple of which the client is investigating, if they are viable and would help the business.</p>
+            <div className="wrapper">
+                <div className="grid-wrapper wrapper">
+                    <div className="col-3">
+                    </div>
+                    <div className="col-12 latest">
+                        <h2>Latest Project</h2>
+                        <p>This client was looking for a static website. Our process involved drawing up inital various designs for the client, before getting feedback and working on the designs interatively.</p>
+                        <p>Once the design phase was complete, we built and hosted the website for the client.
+                           During the process of understanding the clients business, we suggested other ideas where technology could help the clients business. A couple of which the client is investigating, if they are viable and would help the business.</p>
+                    </div>
+                </div>
             </div>
         )
     }
