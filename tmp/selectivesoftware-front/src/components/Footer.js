05759d7c3fceec71682acd41ecd040e0e876808c
NiallBunting <niall.bunting@bjss.com>
2020-04-07 21:00:09 +0100

    Comment values that are relevant right now

--- tmp/selectivesoftware-front/src/components/Footer.js
+++ tmp/selectivesoftware-front/src/components/Footer.js
@@ -1,17 +1,17 @@
 import React from 'react'
 
 class Footer extends React.Component {
+                    //<li>Address: First Line, Second Line, Postcode.</li>
+                    //<li>Company Registration No. 00000000</li>
+                    //<li>Vat Registration GB0000000000</li>
+                    //<li>Registered in England & Wales</li>
     render() {
         return (
             <section id="footer">
                 <ul className="icons">
-                    <li>Address: First Line, Second Line, Postcode.</li>
                     <li>Email: info@selectivesoftware.co.uk</li>
                 </ul>
                 <ul className="copyright">
-                    <li>Company Registration No. 00000000</li>
-                    <li>Vat Registration GB0000000000</li>
-                    <li>Registered in England & Wales</li>
                     <li>&copy; 2020</li>
                     <li>Designed by Selective Software</li>
                 </ul>
